e0fd968356b414e58a2ddb2470a665c73d40bb6e
Ken Choi <kenward97@hotmail.com>
2024-05-31 22:32:22 +0000

    completed express-get exercise - ken choi

---
+++ express-get/express-get-quiz-notes.md
@@ -0,0 +1,38 @@
+# express-get-quiz-notes
+
+## Quiz Questions
+
+Answer the following questions in the provided markdown file before turning in this exercise:
+
+- What is the purpose of the HTTP `GET` method?
+  To get data and return it.
+- What is the appropriate `Content-Type` header for HTTP messages that contain JSON in their bodies?
+  application/json informs the receiving server or client that the content of the message is JSON data, allowing it to parse and handle the data appropriately.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ express-get/server.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+import pg from 'pg';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const db = new pg.Pool({
+  connectionString: 'postgres://dev:dev@localhost/pagila',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+
+app.get('/api/films', async (req, res, next) => {
+  try {
+    const sql = `
+      SELECT "description","filmId","replacementCost","title"
+      FROM "films"
+      ORDER BY "replacementCost" DESC
+      LIMIT 2;
+    `;
+    const result = await db.query(sql);
+    const films = result.rows;
+    res.json(films);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/films/:filmId', async (req, res, next) => {
+  try {
+    const filmId = req.params.filmId;
+    const sql = `
+      SELECT "description","filmId","replacementCost","title"
+      FROM "films"
+      WHERE "filmId" = $1;
+    `;
+    const params = [filmId];
+    const result = await db.query(sql, params);
+    const film = result.rows[0];
+    if (!film) {
+      throw new ClientError(404, `Film with filmId ${filmId} not found`);
+    }
+    res.json(film);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
+
+app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
